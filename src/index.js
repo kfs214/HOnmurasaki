13cc3d5798b841b3dbe383c3949bbd5b97ea51b5
UshiJima (Kazuki, 67-inch, Late 1995) <info@kfs214.net>
2021-03-12 13:33:11 +0900

    add] made custom.scss

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./custom.scss";
 
 // redux
 import { Provider } from "react-redux";
